dfb650f1d25be3443baf0c655b3959e451d9ba70
Chris <chriswong90012@yahoo.com>
2021-08-27 10:30:38 -0700

    component changes

--- client/src/components/EventPage.jsx
+++ client/src/components/EventPage.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const EventPage = () => (
   <div className="eventPage-container">
     <div className="col-8 details">
-      <div className="row trip"></div>
-      <div className="row weather"></div>
+      <div className="row trip" />
+      <div className="row weather" />
       <div className="row event-calendar">
-        <div className="col-6 events"></div>
-        <div className="col-6 calendar"></div>
+        <div className="col-6 events" />
+        <div className="col-6 calendar" />
       </div>
     </div>
     <div className="col-4 activity-selector">
-      <div className="row activity"></div>
+      <div className="row activity" />
     </div>
   </div>
 );
