99093fba62bfa54f6a0f9f38a82349138b556a29
Chris <chriswong90012@yahoo.com>
2021-08-27 18:53:23 -0700

    adding components to dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import Search from './DashboardComponents/Search.jsx';
+import Upcoming from './DashboardComponents/Upcoming.jsx';
+import Past from './DashboardComponents/Past.jsx';
 import './Dashboard.css';
 
 const Dashboard = () => (
   <div className="dashboard-container">
-    <div className="dashboard-search" />
+    <div className="dashboard-search">
+      <Search />
+    </div>
     <div className="dashboard-title">
       <span>Dashboard</span>
     </div>
-    <div className="dashboard-upcoming" />
-    <div className="dashboard-past" />
+    <div className="dashboard-upcoming">
+      <Upcoming />
+    </div>
+    <div className="dashboard-past">
+      <Past />
+    </div>
   </div>
 );
 
