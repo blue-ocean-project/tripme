ab03abd4d4027f4d85dc2cd43574f5bfb27bf374
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-29 22:27:17 -0700

    Add Sample Data

--- SampleData/Dashboard.js
+++ SampleData/Dashboard.js
@@ -5,11 +5,11 @@ const data = {
       destination: 'Honolulu, Hawaii',
       start_date: '2022-01-11T00:00:00.000Z',
       end_date: '2022-01-18T00:00:00.000Z',
-      Members: [
+      members: [
         {
           id: 1,
-          first_name: 'Uncle',
-          last_name: 'Jay',
+          first_name: 'Julian',
+          last_name: 'Yuen',
         },
         {
           id: 2,
@@ -23,16 +23,26 @@ const data = {
       destination: 'Yellowstone, Wyoming',
       start_date: '2022-03-11T00:00:00.000Z',
       end_date: '2022-03-18T00:00:00.000Z',
-      Members: [
+      members: [
         {
           id: 1,
-          first_name: 'Uncle',
-          last_name: 'Jay',
+          first_name: 'Chris',
+          last_name: 'Wong',
         },
         {
           id: 2,
-          first_name: 'Justin',
-          last_name: 'Yuen',
+          first_name: 'Jooyoung',
+          last_name: 'Kim',
+        },
+        {
+          id: 3,
+          first_name: 'Ryan',
+          last_name: 'Chi',
+        },
+        {
+          id: 4,
+          first_name: 'Gi',
+          last_name: 'Tayier',
         },
       ],
     },
@@ -43,16 +53,16 @@ const data = {
       destination: 'Mars, Universe',
       start_date: '2018-01-11T00:00:00.000Z',
       end_date: '2018-01-18T00:00:00.000Z',
-      Members: [
+      members: [
         {
           id: 1,
-          first_name: 'Uncle',
-          last_name: 'Jay',
+          first_name: 'Sean',
+          last_name: 'Lawlor',
         },
         {
           id: 2,
-          first_name: 'Justin',
-          last_name: 'Yuen',
+          first_name: 'Kenneth',
+          last_name: 'Hsu',
         },
       ],
     },
