cefe5db267ec4f868ef0a0903f1e29e7a6c898b2
Chris <chriswong90012@yahoo.com>
2021-08-30 19:00:04 -0700

    fixes to navbar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import NavBar from './NavBar/NavBar.jsx';
 import Dashboard from './Dashboard/Dashboard.jsx';
 import EventPage from './EventPage/EventPage.jsx';
-import actions from '../state/actions/index';
+// import actions from '../state/actions/index';
 import './App.css';
 
 const App = () => {
-  const state = useSelector((states) => states);
-  const dispatch = useDispatch();
+  const state = useSelector((states) => states.changePage);
+  // const dispatch = useDispatch();
 
   return (
     <div className="container-fluid">
