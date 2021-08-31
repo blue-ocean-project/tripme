22558a001476f6c0ecbd946d67261da4cbe4ca0d
Jooyoung Kim <jky0420@gmail.com>
2021-08-30 17:27:19 -0700

    implemented trip-date-calendar column

--- client/src/components/EventPage/EventPageComponents/Trip.jsx
+++ client/src/components/EventPage/EventPageComponents/Trip.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Container, Col, Row, Card, Button } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 import './Trip.css';
 
 const destination = 'hawaii';
+const days = '5';
+const participants = ['Justin', 'Mom', 'Uncle-J'];
 const checkList = ['passport', 'cellphone'];
 
 const Trip = () => (
   <>
     <Container>
       <Row className="eventpage-trip-top">
-        <Button className="eventpage-trip-btn">Invite trip mates</Button>
+        <Button className="eventpage-trip-invite-btn">Invite trip mates</Button>
       </Row>
-      <Row>
-        <Col sm={5} className="justify-content-center flex-column">
+      <Row className="eventpage-trip-bottom">
+        <Col sm={4} className="trip-bot-col-first">
           <div>Destination: {destination}</div>
-          <div>Trip Date:</div>
-          <Card>
-            <Card.Title>say my name</Card.Title>
-          </Card>
+          <div>Trip Date</div>
+          <Row className="e-trip-calendar">
+            <Col sm={3}>
+              <FontAwesomeIcon icon={faCalendarAlt} color="purple" size="2x" />
+            </Col>
+            <Col sm={9}>
+              {days} days {days - 1} nights
+            </Col>
+          </Row>
         </Col>
-        <Col sm={4} className="justify-content-center flex-column">
+        <Col sm={4} className="trip-bot-col-second">
           Participants:
+          <FontAwesomeIcon icon={faUser} color="purple" size="2x" />
         </Col>
-        <Col sm={3} className="justify-content-center flex-column">
+        <Col sm={4} className="trip-bot-col-third">
           <div>Checklist</div>
         </Col>
       </Row>
