08f2fe6583e9a4d17cd95526ccc9d85f1814b993
Jooyoung Kim <jky0420@gmail.com>
2021-08-31 11:57:18 -0700

    implemented icons and names displaying on participants column

--- client/src/components/EventPage/EventPageComponents/Trip.jsx
+++ client/src/components/EventPage/EventPageComponents/Trip.jsx
@@ -9,35 +9,46 @@ const days = '5';
 const participants = ['Justin', 'Mom', 'Uncle-J'];
 const checkList = ['passport', 'cellphone'];
 
-const Trip = () => (
-  <>
-    <Container>
-      <Row className="eventpage-trip-top">
-        <Button className="eventpage-trip-invite-btn">Invite trip mates</Button>
-      </Row>
-      <Row className="eventpage-trip-bottom">
-        <Col sm={4} className="trip-bot-col-first">
-          <div>Destination: {destination}</div>
-          <div>Trip Date</div>
-          <Row className="e-trip-calendar">
-            <Col sm={3}>
-              <FontAwesomeIcon icon={faCalendarAlt} color="purple" size="2x" />
-            </Col>
-            <Col sm={9}>
-              {days} days {days - 1} nights
-            </Col>
-          </Row>
-        </Col>
-        <Col sm={4} className="trip-bot-col-second">
-          Participants:
-          <FontAwesomeIcon icon={faUser} color="purple" size="2x" />
-        </Col>
-        <Col sm={4} className="trip-bot-col-third">
-          <div>Checklist</div>
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+const participantDisplay = function (participantsList) {
+  return participantsList.map((each, i) => (
+    <div className="participant-box" key={i}>
+      <FontAwesomeIcon icon={faUser} color="purple" size="2x" />
+      <span>{each}</span>
+    </div>
+  ));
+};
+
+const Trip = () => {
+  return (
+    <>
+      <Container>
+        <Row className="eventpage-trip-top">
+          <Button className="eventpage-trip-invite-btn">Invite trip mates</Button>
+        </Row>
+        <Row className="eventpage-trip-bottom">
+          <Col sm={4} className="trip-bot-col-first">
+            <div>Destination: {destination}</div>
+            <div>Trip Date</div>
+            <Row className="e-trip-calendar">
+              <Col sm={3}>
+                <FontAwesomeIcon icon={faCalendarAlt} color="purple" size="2x" />
+              </Col>
+              <Col sm={9}>
+                {days} days {days - 1} nights
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={4} className="trip-bot-col-second">
+            Participants:
+            <Row className="participants-container">{participantDisplay(participants)}</Row>
+          </Col>
+          <Col sm={4} className="trip-bot-col-third">
+            <div>Checklist</div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default Trip;
